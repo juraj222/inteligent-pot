de25194df6d066117ebbd3e16829a32fb93f4547
jura-netbook <juraj.vrabec222@gmail.com>
2017-09-11 18:41:04 +0200

    fix controller

--- src/main/java/inteligentpot/com/inteligentpot/controller/potController.java
+++ src/main/java/inteligentpot/com/inteligentpot/controller/potController.java
@@ -2,11 +2,15 @@ package inteligentpot.com.inteligentpot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/main")
 public class potController {
-	    @RequestMapping("/")
-	    public String index() {
+	
+		@RequestMapping(method=RequestMethod.GET)
+	    public @ResponseBody String index() {
 	        return "Greetings from Spring Boot!";
 	    }
 
