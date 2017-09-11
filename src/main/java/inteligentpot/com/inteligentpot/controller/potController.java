7d7b60ee64c07490da0e08b9ab26f10937a8ed32
jura-netbook <juraj.vrabec222@gmail.com>
2017-09-11 18:28:48 +0200

    fix dependency problem

--- src/main/java/inteligentpot/com/inteligentpot/controller/potController.java
+++ src/main/java/inteligentpot/com/inteligentpot/controller/potController.java
@@ -1,9 +1,9 @@
 package inteligentpot.com.inteligentpot.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class potController {
 	    @RequestMapping("/")
 	    public String index() {
