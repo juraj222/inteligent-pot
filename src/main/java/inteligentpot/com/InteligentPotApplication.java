0ef87e31f6a89a643ef305db568d8f68892016c7
jura-netbook <juraj.vrabec222@gmail.com>
2017-09-20 17:27:12 +0200

    fix table drop

--- src/main/java/inteligentpot/com/InteligentPotApplication.java
+++ src/main/java/inteligentpot/com/InteligentPotApplication.java
@@ -2,9 +2,11 @@ package inteligentpot.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication(scanBasePackages = {
 		"inteligentpot.com.core.service,inteligentpot.com.core.config, inteligentpot.com.controller" })
+@EntityScan(basePackages = { "inteligentpot.com.core.domain" })
 public class InteligentPotApplication {
 
 	public static void main(String[] args) {
